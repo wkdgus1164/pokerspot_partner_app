240039098c4ab367bc56282402e8e33bbf66d65b
wkdgus1164 <wkdgus1164@naver.com>
2023-06-07 19:20:15 +0900

    feat: Picker Dialog 추가

---
+++ lib/common/components/dialogs/picker_dialog.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pokerspot_partner_app/common/constants/sizes.dart';
+import 'package:pokerspot_partner_app/common/theme/typography.dart';
+
+class PickerDialog extends StatelessWidget {
+  final String title;
+  final List<String> selections;
+  final VoidCallback onCancel;
+  final VoidCallback onSubmit;
+  final ValueChanged<int>? onItemChanged;
+
+  const PickerDialog({
+    super.key,
+    required this.title,
+    required this.selections,
+    required this.onCancel,
+    required this.onSubmit,
+    this.onItemChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(8),
+        ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const SizedBox(height: padding24),
+          Text(title, style: titleSmall),
+          SizedBox(
+            width: double.infinity,
+            height: 160,
+            child: CupertinoPicker(
+              itemExtent: 50,
+              onSelectedItemChanged: onItemChanged,
+              children: selections
+                  .map((selection) => Container(
+                        height: 50,
+                        alignment: Alignment.center,
+                        child: Text(
+                          selection,
+                          style: bodySmall.copyWith(color: Colors.black),
+                        ),
+                      ))
+                  .toList(),
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: OutlinedButton(
+                  onPressed: () {},
+                  child: const Text('취소'),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('선택 완료'),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/presentation/views/member/login/views/login_view.dart
+++ lib/presentation/views/member/login/views/login_view.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:pokerspot_partner_app/common/components/dialogs/picker_dialog.dart';
 import 'package:pokerspot_partner_app/common/constants/sizes.dart';
 import 'package:pokerspot_partner_app/common/routes/base/bottom_navigation.dart';
 import 'package:pokerspot_partner_app/common/routes/base/member.dart';
 import 'package:pokerspot_partner_app/common/theme/color.dart';
 import 'package:pokerspot_partner_app/common/theme/typography.dart';
+import 'package:pokerspot_partner_app/data/utils/logger.dart';
 import 'package:pokerspot_partner_app/locator.dart';
 import 'package:pokerspot_partner_app/presentation/dialog/toast.dart';
 import 'package:pokerspot_partner_app/presentation/providers/partner_provider.dart';
@@ -45,6 +47,11 @@ class LoginView extends StatelessWidget {
                   if (result && context.mounted) {
                     context.replaceNamed(BottomNavigationRoutes.home.path);
                   } else {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) =>
+                          _buildStartTimePickerDialog(),
+                    );
                     showToast(context: context, message: '로그인 실패하였습니다.');
                   }
                 },
@@ -79,6 +86,43 @@ class LoginView extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+
+  PickerDialog _buildStartTimePickerDialog() {
+    return PickerDialog(
+      onCancel: () {},
+      onSubmit: () {},
+      onItemChanged: (value) {
+        Logger.d(value);
+      },
+      selections: const [
+        '00:00',
+        '00:30',
+        '01:00',
+        '01:30',
+        '02:00',
+        '02:30',
+        '03:00',
+        '03:30',
+        '04:00',
+        '04:30',
+        '05:00',
+        '05:30',
+        '06:00',
+        '06:30',
+        '07:00',
+        '07:30',
+        '08:00',
+        '08:30',
+        '09:00',
+        '09:30',
+        '10:00',
+        '10:30',
+        '11:00',
+        '11:30',
+      ],
+      title: '오픈시간 선택',
     );
   }
 }
